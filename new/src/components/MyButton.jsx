060df54ae3e06374e17fdf85147d283bf152bc51
Abdul Muiz <abdulmuizpk@gmail.com>
2024-09-09 15:19:35 +0500

    .

--- new/src/components/MyButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyButton = () => {
-  return <button className='btn btn-success' onClick={() => alert('Button clicked!')}>Click me</button>;
-};
-
-export default MyButton;
--- new/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reactToWebComponent from 'react-to-webcomponent';
-import DropdownMenu from './components/DropdownMenu';
-import MyButton from './components/MyButton';
-
-customElements.define('dropdown-menu', reactToWebComponent(DropdownMenu, React, ReactDOM));
-customElements.define('my-button', reactToWebComponent(MyButton, React, ReactDOM));
