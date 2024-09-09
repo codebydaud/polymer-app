d72a52c2b130031b63b3bc58a34e33fab91f5f05
Syed Muhammad Daud Rizvi <daudrizvi0@gmail.com>
2024-09-09 14:41:44 +0500

    .

--- new/src/components/MyButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyButton = () => {
-  return <button id='button' onClick={() => alert('Button clicked!')}>Click me</button>;
-};
-
-export default MyButton;
--- new/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reactToWebComponent from 'react-to-webcomponent';
-import HelloWorld from './components/HelloWorld';
-import MyButton from './components/MyButton';
-
-customElements.define('hello-world', reactToWebComponent(HelloWorld, React, ReactDOM));
-customElements.define('my-button', reactToWebComponent(MyButton, React, ReactDOM));
